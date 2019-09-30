11ce643c50ac17bb02364e0754f0863075595796
Michele Zucca <michele.zucca81@gmail.com>
2019-10-01 00:10:43 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,38 @@
+//AOS 
+AOS.init({
+    offset: 400,
+    duration: 1000
+});
+
+const heads = document.querySelector('.heads');
+const tails = document.querySelector('.tails');
+const buttonFlip = document.querySelector('.btn-flip');
+let headsScore = document.querySelector('.heads-rolls').textContent ='0';
+let tailsScore = document.querySelector('.tails-rolls').textContent = '0';
+let scoreContent = document.querySelector('.flip-times');
+let gameTitle = document.querySelector('.title');
+
+function flipCoin() {
+
+    const newFlip = ((Math.floor(Math.random() * 2) === 0) ? heads : tails); 
+    if(newFlip === heads) {
+        console.log('heads');
+        heads.classList.remove('hide');
+        tails.classList.add('hide');
+        scoreContent.textContent = `You flipped ${headsScore++ +1} heads and ${tailsScore} tails`;
+                
+    } else if(newFlip === tails){
+        console.log('tails');
+        tails.classList.remove('hide');
+        heads.classList.add('hide');
+        scoreContent.textContent = `You flipped ${headsScore} heads and ${tailsScore++ +1} tails`;
+
+    }          
+
+}
+
+buttonFlip.addEventListener('click', flipCoin);
+
+
+
+
